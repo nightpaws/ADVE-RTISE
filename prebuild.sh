e3e6dac77aef540c2e6a9aff2649c7eb8bfabba5
nightpaws <nightpaws@users.noreply.github.com>
2016-01-15 23:33:51 +0000

    added basic configs to prebuild script for bare essentials before starting

--- prebuild.sh
+++ prebuild.sh
@@ -1,1 +1,22 @@
- 
+#Configure Global Requirements. Please note that sudo is required to run this.
+echo Configuring NPM...
+
+if [ "$(whoami)" != "root" ]
+then
+echo You are not running as root. This script cannot run... \(Try sudo\)
+else
+#Streaming build system
+npm install --global gulp
+#installs gulp in project devDependencies
+npm install --save-dev gulp
+#File/Directory Deletion Utility
+npm install --save-dev del
+#Functions for gulp addons
+npm install --save-dev gulp-util
+#Front end package Manager
+npm install --global bower
+
+#Required to allow restart/persistent running. Crontab this to run on boot
+npm install --global forever
+echo Script has finished running. Thanks!
+	fi
