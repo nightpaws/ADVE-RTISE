400f24b5ca9fafd0e11bc25c56eb49a521dbd90a
Nightpaws <nightpaws@users.noreply.github.com>
2016-01-18 23:31:34 +0000

    Added more initial files and UML documentation. Updated .gitignore properly Commented out some things I won't need till later.

--- build.sh
+++ build.sh
@@ -1,1 +1,30 @@
+# echo Performing client build
+# #Starting Client Build
+# cd src/client/src
+# npm install
+# bower install
+# bower update
+# gulp build
+# cd ../../..
+
+# #Starting Server Build
+# echo Performing server build
+# cd src/server
+# npm install
+# cd ../..
+
+# echo Performing deployment
+# gulp build
+
+# echo Performing the failing run of the application
+# cd ./build
+# echo This will fail
+# node index.js
+# echo See, It will work now
+
+# echo ------------------------------------------------------------$
+# echo DONE
+# echo You may now sort the certs out, drop in a custom config and $
+# echo run "cd ./build" & "node index.js" to make things go
+
  
