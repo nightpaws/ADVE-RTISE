d5be979d764eed8373c9c45a36bab6a46cfc3b8e
Nightpaws <nightpaws@users.noreply.github.com>
2016-01-26 01:39:24 +0000

    Wrote initial build scripts. Not functional... yet.

---
+++ build.cmd
@@ -0,0 +1,28 @@
+ echo Performing client build
+ :: Starting Client Build
+ cd src/frontend/src
+ call npm install
+ call bower install
+ call bower update
+ call gulp build
+ cd ../../..
+
+:: Starting Server Build
+ echo Performing server build
+ cd src/backend
+ call npm install
+ cd ../..
+
+ echo Performing deployment
+ call gulp build
+
+ echo Performing the failing run of the application
+ echo This will fail as it must create files...
+ cd ./build
+ call node index.js
+ echo It should work now...
+
+ echo ------------------------------------------------------------$
+ echo DONE
+ echo You may now sort the certs out, drop in a custom config and $
+ echo run "cd ./build" & "node index.js" to make things go
--- build.sh
+++ build.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
-# echo Performing client build
-# #Starting Client Build
-# cd src/client/src
-# npm install
-# bower install
-# bower update
-# gulp build
-# cd ../../..
+ echo Performing client build
+ #Starting Client Build
+ cd src/client/src
+ npm install
+ bower install
+ bower update
+ gulp build
+ cd ../../..
 
-# #Starting Server Build
-# echo Performing server build
-# cd src/server
-# npm install
-# cd ../..
+ #Starting Server Build
+ echo Performing server build
+ cd src/server
+ npm install
+ cd ../..
 
-# echo Performing deployment
-# gulp build
+ echo Performing deployment
+ gulp build
 
-# echo Performing the failing run of the application
-# cd ./build
-# echo This will fail
-# node index.js
-# echo See, It will work now
+ echo Performing the failing run of the application
+ echo This will fail as it must create files...
+ cd ./build
+ node index.js
+ echo It should work now...
 
-# echo ------------------------------------------------------------$
-# echo DONE
-# echo You may now sort the certs out, drop in a custom config and $
-# echo run "cd ./build" & "node index.js" to make things go
+ echo ------------------------------------------------------------$
+ echo DONE
+ echo You may now sort the certs out, drop in a custom config and $
+ echo run "cd ./build" & "node index.js" to make things go
 
  
