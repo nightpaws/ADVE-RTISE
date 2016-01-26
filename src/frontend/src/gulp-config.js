6b685046a397869afcea516c7db0c66f88638990
Nightpaws <nightpaws@users.noreply.github.com>
2016-01-26 01:40:36 +0000

    Configured Bower, and configured gulp (mostly)

--- src/frontend/src/gulp-config.js
+++ src/frontend/src/gulp-config.js
@@ -1,3 +1,90 @@
 /**
- * Created by Nightpaws on 20/01/2016.
+ * Created by Nightpaws on 25/01/2016.
  */
+module.exports = function () {
+
+    //Location Configs. Stores paths of files and directories for use
+
+    var buildPath = '../build/';
+
+    var config = {
+        bower:{
+            json: require('./bower.json'),
+            src: './bower_components/',
+            ignorePath: '../..'
+        },
+        build: {
+            dir: buildPath,
+            cssDir: buildPath + 'style/',
+            jsDir: buildPath + 'js/',
+            libDir: buildPath + 'lib/'
+        },
+        favicon:{
+            src: ['./favicon.ico']
+        },
+        fonts: {
+            dir: ['./fonts/**']
+        },
+        html: {
+            src: ['./app/**/*.html', '!./index.html']
+        },
+        ignore: {
+            dir: ['!./bower_components/**/*', '!./node_modules/**/*']
+        },
+        index: 'index.html',
+        js: {},
+        sass: {
+            src: './style/style.scss'
+        }
+    };
+
+
+    //Get Inject and WireDep Defaults
+
+
+//Configuration for Building with WireDep
+
+    config.getWiredepDefault = function() {
+
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.build.libDir,
+            ignorePath: config.bower.ignorePath,
+            fileTypes: {
+                html: {
+                    replace: {
+                        js: function(filePath) {
+                            return '<script src="/dashboard/' + filePath.replace('../build/', '') + '"></script>';
+                        },
+                        css: function(filePath) {
+                            return '<link rel="stylesheet" href="/dashboard/' + filePath.replace('../build/', '') + '"/>';
+                        }
+                    }
+                }
+            }
+        };
+
+        return options;
+    };
+
+    config.getInjectDefault = function() {
+
+        var options = {
+            addRootSlash: false,
+            transform: function(filePath, file, i, length) {
+
+                if(filePath.endsWith('.css')){
+                    return '<link rel="stylesheet" type="text/css" href="/dashboard/' + filePath.replace('../build/', '') + '">';
+                }
+
+                return '<script src="/dashboard/' + filePath.replace('../build/', '') + '"></script>';
+            }
+        };
+
+        return options;
+
+    };
+
+    return config;
+
+};
--- src/frontend/src/gulpfile.js
+++ src/frontend/src/gulpfile.js
@@ -16,27 +16,107 @@ gulp.task('clearbuild', function(cb){
     });
 });
 
-//Configuration for Building with WireDep
+//Build the SASS - (sassiness is imperative)
+gulp.task('sass', ['clearbuild'], function() {
 
-config.getWiredepDefault = function() {
+    log('Compiling SASS...');
 
-    var options = {
-        bowerJson: config.bower.json,
-        directory: config.build.libDir,
-        ignorePath: config.bower.ignorePath,
-        fileTypes: {
-            html: {
-                replace: {
-                    js: function(filePath) {
-                        return '<script src="/dashboard/' + filePath.replace('../build/', '') + '"></script>';
-                    },
-                    css: function(filePath) {
-                        return '<link rel="stylesheet" href="/dashboard/' + filePath.replace('../build/', '') + '"/>';
-                    }
-                }
+    return gulp
+        .src(config.sass.srcdir)
+        .pipe($.sass({errLogToConsole: true}))
+        .pipe(gulp.dest(config.build.cssDir));
+});
+
+//Bulk dependency handler. Just a function caller...
+gulp.task('handleDep', ['clearbuild', 'libcopy', 'dep'], function(){});
+
+//Copy Libraries
+gulp.task('libcopy',['clearbuild'], function(){
+
+    log("Copying Bower Dependencies to: " + config.build.libDir);
+
+    return gulp
+        .src(config.bower.srcDir + '**/*.*')
+        .pipe(gulp.dest(config.build.libDir));
+
+});
+
+//Inject Dependencies
+gulp.task('dep', ['clearbuild', 'libcopy', 'sass', 'copy'], function() {
+
+    log('Injecting dependencies for:' +
+        ' \n\t\t-bower components ' +
+        ' \n\t\t-custom js' +
+        ' \n\t\t-custom css');
+
+    var options = config.getWiredepDefault();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src([config.build.buildDir + 'app/**/*.module.js', config.build.buildDir + 'app/**/*.js', '!' + config.build.buildDir + 'app/app.js']), config.getInjectDefault()))
+        .pipe($.inject(gulp.src(config.build.cssDir + '**/*.css'), config.getInjectDefault()))
+        .pipe(gulp.dest(config.build.buildDir));
+});
+
+//Copy Full Application
+gulp.task('copy', ['clearbuild', 'copyfavicon', 'copyFonts'], function() {
+
+    log('Copying Application to: ' + config.build.builddir);
+
+    return gulp
+        .src(config.js.srcDir.concat(config.html.srcDir).concat(config.ignore.ignoreDir))
+        .pipe(gulp.dest(config.build.buildDir + "app/"));
+});
+
+//Copy Favicon
+gulp.task('copyfavicon', ['clearbuild'], function() {
+
+    log('Copying Favicon to: ' + config.build.builddir);
+
+    return gulp
+        .src(config.favicon.src)
+        .pipe(gulp.dest(config.build.buildDir));
+});
+
+//Copy Fonts
+gulp.task('copyFonts', ['clearbuild'], function() {
+
+    log('Copying Fonts to: ' + config.build.builddir);
+    return gulp
+        .src(config.fonts.src)
+        .pipe(gulp.dest(config.build.buildDir + '/fonts/'));
+});
+
+//Full Build Operation
+gulp.task('build',['clearbuild', 'sass', 'handleDep', 'copy'], function(){
+    log($.util.colors.green('---Build has Completed.---'));
+});
+
+//Adding Log Function
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
             }
         }
-    };
+    } else {
+        $.util.log($.util.colors.gray(msg));
+    }
+}
 
-    return options;
-};
+    //Apply Polyfill function to add endsWith functionality since endsWith has not yet been implemented in JS.
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+    if (!String.prototype.endsWith) {
+        String.prototype.endsWith = function(searchString, position) {
+            var subjectString = this.toString();
+            if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+                position = subjectString.length;
+            }
+            position -= searchString.length;
+            var lastIndex = subjectString.indexOf(searchString, position);
+            return lastIndex !== -1 && lastIndex === position;
+        };
+}
