2ef81a63ea909c9149a8f2af281c2d2ceb35d999
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-02-04 23:51:45 +0000

    Amended SQL code to reflect new database relation

--- docs/SQL Scripts/createDB.sql
+++ docs/SQL Scripts/createDB.sql
@@ -1,64 +1,58 @@
 CREATE TABLE Account (
-  id       INT(11)      NOT NULL AUTO_INCREMENT,
-  website  VARCHAR(255) NOT NULL,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  year     DATETIME     NOT NULL,
-  enabled  TINYINT      NOT NULL,
-  PRIMARY KEY (id)
-);
+  id       int(11) NOT NULL AUTO_INCREMENT, 
+  website  varchar(255) NOT NULL, 
+  username varchar(255) NOT NULL, 
+  password varchar(255) NOT NULL, 
+  year     datetime NOT NULL, 
+  enabled  tinyint NOT NULL, 
+  PRIMARY KEY (id));
 CREATE TABLE AccountMessages (
-  id        INT(11) NOT NULL AUTO_INCREMENT,
-  Accountid INT(11) NOT NULL,
-  Messageid INT(11) NOT NULL,
-  PRIMARY KEY (id)
-);
+  id        int(11) NOT NULL AUTO_INCREMENT, 
+  Accountid int(11) NOT NULL, 
+  Messageid int(11) NOT NULL, 
+  Staffid   int(11) NOT NULL, 
+  PRIMARY KEY (id));
 CREATE TABLE Class (
-  id          INT(11)      NOT NULL AUTO_INCREMENT,
-  code        VARCHAR(255) NOT NULL UNIQUE,
-  description VARCHAR(255) NOT NULL,
-  year        INT(11)      NOT NULL,
-  PRIMARY KEY (id)
-);
+  id          int(11) NOT NULL AUTO_INCREMENT, 
+  code        varchar(255) NOT NULL UNIQUE, 
+  description varchar(255) NOT NULL, 
+  year        int(11) NOT NULL, 
+  PRIMARY KEY (id));
 CREATE TABLE ClassStaff (
-  id      INT(11) NOT NULL AUTO_INCREMENT,
-  Staffid INT(11) NOT NULL,
-  Classid INT(11) NOT NULL,
-  PRIMARY KEY (id)
-);
+  id      int(11) NOT NULL AUTO_INCREMENT, 
+  Staffid int(11) NOT NULL, 
+  Classid int(11) NOT NULL, 
+  PRIMARY KEY (id));
 CREATE TABLE ClassStudent (
-  id        INT(11) NOT NULL AUTO_INCREMENT,
-  Studentid INT(11) NOT NULL,
-  Classid   INT(11) NOT NULL,
-  PRIMARY KEY (id)
-);
+  id        int(11) NOT NULL AUTO_INCREMENT, 
+  Studentid int(11) NOT NULL, 
+  Classid   int(11) NOT NULL, 
+  PRIMARY KEY (id));
 CREATE TABLE Message (
-  id      INT(11)      NOT NULL AUTO_INCREMENT,
-  subject VARCHAR(255) NOT NULL,
-  message TEXT,
-  `date`  DATETIME     NOT NULL,
-  PRIMARY KEY (id)
-);
+  id      int(11) NOT NULL AUTO_INCREMENT, 
+  subject varchar(255) NOT NULL, 
+  message text, 
+  `date`  datetime NOT NULL, 
+  PRIMARY KEY (id));
 CREATE TABLE Staff (
-  id       INT(11)      NOT NULL AUTO_INCREMENT,
-  forename VARCHAR(255) NOT NULL,
-  surname  VARCHAR(255) NOT NULL,
-  username VARCHAR(255) NOT NULL UNIQUE,
-  mail     VARCHAR(255) NOT NULL UNIQUE,
-  active   DATETIME     NOT NULL,
-  PRIMARY KEY (id)
-);
+  id       int(11) NOT NULL AUTO_INCREMENT, 
+  forename varchar(255) NOT NULL, 
+  surname  varchar(255) NOT NULL, 
+  username varchar(255) NOT NULL UNIQUE, 
+  mail     varchar(255) NOT NULL UNIQUE, 
+  active   datetime NOT NULL, 
+  PRIMARY KEY (id));
 CREATE TABLE Student (
-  id         INT(11)           NOT NULL AUTO_INCREMENT,
-  forename   VARCHAR(255)      NOT NULL,
-  surname    VARCHAR(255)      NOT NULL,
-  username   VARCHAR(255)      NOT NULL UNIQUE,
-  regnum     INT(11)           NOT NULL UNIQUE,
-  mail       VARCHAR(255)      NOT NULL UNIQUE,
-  active     DATETIME          NOT NULL,
-  subscribed TINYINT DEFAULT 0 NOT NULL,
-  PRIMARY KEY (id)
-);
+  id         int(11) NOT NULL AUTO_INCREMENT, 
+  forename   varchar(255) NOT NULL, 
+  surname    varchar(255) NOT NULL, 
+  username   varchar(255) NOT NULL UNIQUE, 
+  regnum     int(11) NOT NULL UNIQUE, 
+  mail       varchar(255) NOT NULL UNIQUE, 
+  active     datetime NOT NULL, 
+  subscribed tinyint DEFAULT 0 NOT NULL, 
+  PRIMARY KEY (id));
+ALTER TABLE AccountMessages ADD INDEX FKAccountMes916879 (Staffid), ADD CONSTRAINT FKAccountMes916879 FOREIGN KEY (Staffid) REFERENCES Staff (id);
 ALTER TABLE AccountMessages ADD INDEX FKAccountMes944452 (Accountid), ADD CONSTRAINT FKAccountMes944452 FOREIGN KEY (Accountid) REFERENCES Account (id);
 ALTER TABLE AccountMessages ADD INDEX FKAccountMes653056 (Messageid), ADD CONSTRAINT FKAccountMes653056 FOREIGN KEY (Messageid) REFERENCES Message (id);
 ALTER TABLE ClassStudent ADD INDEX FKClassStude673879 (Studentid), ADD CONSTRAINT FKClassStude673879 FOREIGN KEY (Studentid) REFERENCES Student (id);
