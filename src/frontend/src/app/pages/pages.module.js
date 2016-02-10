14c597f1464f1f589734edfafc3c0f644716899b
Nightpaws <nightpaws@users.noreply.github.com>
2016-02-10 12:22:35 +0000

    Fixed links from pages

--- src/frontend/src/app/pages/pages.module.js
+++ src/frontend/src/app/pages/pages.module.js
@@ -1,3 +1,23 @@
 /**
  * Created by Nightpaws on 10/02/2016.
  */
+angular.module('pages', [])
+    .config(['$stateProvider',function($stateProvider) {
+
+        $stateProvider
+            .state('page.about', {
+                url: 'about/',
+                templateUrl: 'app/pages/about.html',
+                onEnter: function($rootScope){
+                    $rootScope.app.stateTitle = 'Help';
+                }
+            })
+            .state('page.help', {
+                url: 'help/',
+                templateUrl: 'app/pages/help.html',
+                onEnter: function($rootScope){
+                    $rootScope.app.stateTitle = 'Help';
+                }
+            })
+
+    }]);
