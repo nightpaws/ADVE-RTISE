51cc2b69f3c32ee83162ed0e2d2aca54793533ce
Nightpaws <nightpaws@users.noreply.github.com>
2016-02-11 14:35:17 +0000

    Fixing broken reference called app advertise and advertise-app in different places

--- src/frontend/src/app/app.js
+++ src/frontend/src/app/app.js
@@ -1,4 +1,4 @@
 /**
  * Created by Nightpaws on 26/01/2016.
  */
-var app = angular.module('advertise-app', ['advertise.config','polyfill','navigation','user','ui-router','ui.bootstrap','LocalStorageModule', 'ngAnimate', 'ng.deviceDetector','infinite-scroll']);
+var app = angular.module('advertise', ['advertise.config','polyfill','navigation','user','ui-router','ui.bootstrap','LocalStorageModule', 'ngAnimate', 'ng.deviceDetector','infinite-scroll']);
