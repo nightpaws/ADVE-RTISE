a262e315f785b25f954617ab2d6d5a935b7176a4
Nightpaws <nightpaws@users.noreply.github.com>
2016-02-11 15:30:00 +0000

    Added app structure to allow linking and execution of angular modules (hopefully)

--- src/frontend/src/app/app.js
+++ src/frontend/src/app/app.js
@@ -1,4 +1,93 @@
 /**
  * Created by Nightpaws on 26/01/2016.
  */
-var app = angular.module('advertise', ['advertise.config','polyfill','navigation','user','ui-router','ui.bootstrap','LocalStorageModule', 'ngAnimate', 'ng.deviceDetector','infinite-scroll']);
+var app = angular.module('advertise', ['advertise.config', 'pages', 'polyfill', 'header', 'navigation', 'session', 'user', 'ui-router',
+    'ui.bootstrap', 'LocalStorageModule', 'ngAnimate', 'ng.deviceDetector', 'infinite-scroll', 'toastr']);
+
+app.config(['$stateProvider', '$locationProvider', '$urlMatcherFactoryProvider', '$urlRouterProvider', function ($stateProvider, $locationProvider, $urlMatcherFactory, $urlRouterProvider) {
+
+    $urlMatcherFactory.caseInsensitive(true);
+    $urlMatcherFactory.strictMode(false);
+
+    $urlRouterProvider.otherwise('/404');
+
+    $stateProvider
+        .state('page', {
+            url: '/',
+            abstract: true,
+            views: {
+                'header': {
+                    templateUrl: 'app/header/header.html',
+                    controller: 'header.controller'
+                },
+                'nav': {
+                    templateUrl: 'app/navigation/navigation.html',
+                    controller: 'nav'
+                },
+                'main': {
+                    template: '<div ui-view style="height: 100%"></div> '
+                }
+            }
+        })
+        .state('404', {
+            url: '/404',
+            views: {
+                'header': {
+                    template: ''
+                },
+                'nav': {
+                    template: ''
+                },
+                'main': {
+                    templateUrl: 'app/pages/404.html'
+                }
+            },
+            onEnter: function ($rootScope) {
+                $rootScope.app.showHeader = false;
+                $rootScope.app.showNav = false;
+            },
+            onExit: function ($rootScope) {
+                $rootScope.app.showHeader = true;
+                $rootScope.app.showNav = true;
+            }
+        });
+
+    $locationProvider.html5Mode(true);
+}]);
+
+app.run(['$rootScope', '$state', '$location', 'user.service', '$timeout', function($rootScope, $state, $location, userService, $timeout){
+
+    userService.loadUser();
+
+    //print ui-router errors
+    $rootScope.$on("$stateChangeError", console.log.bind(console));
+
+    $rootScope.app = {
+        showNav: true,
+        showHeader: true
+    };
+
+    $timeout(function(){
+        $rootScope.app.loaded = true;
+    }, 3000);
+
+    $rootScope.$on("$stateChangeStart", function(args){
+        if(!userService.getUser()){
+            $location.path('/user/login');
+        }
+    });
+
+    $rootScope.$on("$stateChangeSuccess", function(args){
+
+        var name = $state.current.name;
+
+        switch (name){
+            case 'application':
+                $rootScope.app.displayBack = false;
+                break;
+            default:
+                $rootScope.app.displayBack =  true;
+        }
+    });
+
+}]);
