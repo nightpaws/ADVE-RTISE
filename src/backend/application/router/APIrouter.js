99d956b7b658953cc7e90da5d4224a26e412ea78
Nightpaws <nightpaws@users.noreply.github.com>
2016-02-26 16:40:43 +0000

    Casing issue where windows likes mixed case. linux doesnt. FUuuuuuuuuuuuuuuuuuuuuuuuu..... Fixed more but mongo won't run on windows 10.

--- src/backend/application/router/APIrouter.js
+++ src/backend/application/router/APIrouter.js
@@ -10,18 +10,18 @@ var apirouter = function(){
     var apirouter = express.Router();
 
     var userRouter = require('./users')();
-    APIrouter.use('/users', userRouter);
+    apirouter.use('/users', userRouter);
 
     var appRouter = require('./apps')();
-    APIrouter.use('/application', appRouter);
+    apirouter.use('/application', appRouter);
 
     var accountRouter = require('./accounts')();
-    APIrouter.use('/accounts',accountRouter);
+    apirouter.use('/accounts',accountRouter);
 
     var adminRouter = require('./admin')();
-    APIrouter.use('/admin',adminRouter);
+    apirouter.use('/admin',adminRouter);
 
-    APIrouter.use('*', function(req, res){
+    apirouter.use('*', function(req, res){
 
         res.status(404).send('api not found');
 
