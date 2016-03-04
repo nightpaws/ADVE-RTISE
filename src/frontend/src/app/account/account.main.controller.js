86cb900741611e1d463c4d024bbd1f062b2cf76a
Nightpaws <nightpaws@users.noreply.github.com>
2016-03-04 17:03:34 +0000

    Cleanup of workspace. Determining redundant code to remove

--- src/frontend/src/app/account/account.main.controller.js
+++ src/frontend/src/app/account/account.main.controller.js
@@ -4,27 +4,27 @@
 angular.module('account')
     .controller('account.main.controller', ['$scope', 'account.request.factory', 'toastr', function($scope, requestHelper, toastr){
 
-        $scope.warnings = {
-            //scope: [],
-            //otherscope: []
-        };
-
-        requestHelper.getInterfaceInfo()
-            .then(function(data){
-
-                if(!data.data.successful){
-                    toastr.error(data.data.message, 'Error');
-                }else{
-
-                    //Toastr Messages go here
-
-                    toastr.error('Unknown warning type', 'Error');
-                }
-
-            })
-            .catch(function(data){
-                toastr.error('An error occurred while connecting to the server.', 'Error');
-            })
+        //$scope.warnings = {
+        //    //scope: [],
+        //    //otherscope: []
+        //};
+        //
+        //requestHelper.getInterfaceInfo()
+        //    .then(function(data){
+        //
+        //        if(!data.data.successful){
+        //            toastr.error(data.data.message, 'Error');
+        //        }else{
+        //
+        //            //Toastr Messages go here
+        //
+        //            toastr.error('Unknown warning type', 'Error');
+        //        }
+        //
+        //    })
+        //    .catch(function(data){
+        //        toastr.error('An error occurred while connecting to the server.', 'Error');
+        //    })
 
 
     }]);
--- src/frontend/src/app/interface/interface.main.controller.js
+++ src/frontend/src/app/interface/interface.main.controller.js
@@ -4,27 +4,27 @@
 angular.module('interface')
     .controller('interface.main.controller', ['$scope', 'interface.request.factory', 'toastr', function($scope, requestHelper, toastr){
 
-        $scope.warnings = {
-            //scope: [],
-            //otherscope: []
-        };
-
-        requestHelper.getInterfaceInfo()
-            .then(function(data){
-
-                if(!data.data.successful){
-                    toastr.error(data.data.message, 'Error');
-                }else{
-
-                    //Toastr Messages go here
-
-                    toastr.error('Unknown warning type', 'Error');
-                }
-
-            })
-            .catch(function(data){
-                toastr.error('An error occurred while connecting to the server.', 'Error');
-            })
+        //$scope.warnings = {
+        //    //scope: [],
+        //    //otherscope: []
+        //};
+        //
+        //requestHelper.getInterfaceInfo()
+        //    .then(function(data){
+        //
+        //        if(!data.data.successful){
+        //            toastr.error(data.data.message, 'Error');
+        //        }else{
+        //
+        //            //Toastr Messages go here
+        //
+        //            toastr.error('Unknown warning type', 'Error');
+        //        }
+        //
+        //    })
+        //    .catch(function(data){
+        //        toastr.error('An error occurred while connecting to the server.', 'Error');
+        //    })
 
 
     }]);
