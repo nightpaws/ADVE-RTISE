92f2af63f5fe6bb2aa910ada3185a814b76d1cae
Nightpaws <nightpaws@users.noreply.github.com>
2016-03-07 16:18:25 +0000

    checkbox processing

--- src/frontend/src/app/interface/interface.main.controller.js
+++ src/frontend/src/app/interface/interface.main.controller.js
@@ -2,7 +2,52 @@
  * Created by nightpaws on 10/02/2016.
  */
 angular.module('interface')
-    .controller('interface.main.controller', ['$scope', 'interface.request.factory', 'toastr', function($scope, requestHelper, toastr){
+    .controller('interface.main.controller', ['$scope', 'interface.request.factory', 'toastr', function ($scope, requestHelper, toastr) {
+
+        //Checkbox handling
+        $scope.Years = [
+            {
+                group: "1st Year"
+            },
+            {
+                group: "2nd Year"
+            },
+            {
+                group: "3rd Year"
+            },
+            {
+                group: "4th Year"
+            },
+            {
+                group: "5rd Year"
+            },
+            {
+                group: "PhD Year"
+            }
+        ];
+
+        $scope.checkAll = function () {
+            if ($scope.selectedAll) {
+                $scope.selectedAll = true;
+            } else {
+                $scope.selectedAll = false;
+            }
+            angular.forEach($scope.classes, function (year) {
+                year.Selected = $scope.selectedAll;
+            });
+        };
+
+
+        $scope.messages = {
+            subject: null,
+            message: null,
+            years: null
+        };
+
+        $scope.sendMessage = function () {
+
+        };
+
 
         //$scope.warnings = {
         //    //scope: [],
