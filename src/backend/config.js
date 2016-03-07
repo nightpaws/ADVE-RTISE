6a3e6ad50633c4500a2d7f1e4a7aa79d788e0404
Nightpaws <nightpaws@users.noreply.github.com>
2016-03-07 15:37:11 +0000

    Rebuilt config

--- src/backend/config.js
+++ src/backend/config.js
@@ -4,9 +4,9 @@
 var config = {
     //Constructing the Node Server.
 
-    ports:{
-        https:3001,
-        http:3000
+    ports: {
+        https: 3001,
+        http: 3000
     },
 
     ssl: {
@@ -20,12 +20,12 @@ var config = {
         src: 'public/favicon.ico'
     },
 
-    mongoDB:{
+    mongoDB: {
         string: 'mongodb://localhost/advertise'
     },
 
     protect: {
-        ignore: [ 
+        ignore: [
             '/users/auth',
             '/users/register'
         ]
@@ -37,11 +37,61 @@ var config = {
     },
 
     twitter: {
-        app: {
+        //If login is implemented this app can be used to auth for all users. otherwise separate apps required per account.
+        //Generate tokens at apps.twitter.com
+        //Login implementation would use https://www.npmjs.com/package/passport-twitter
+        //app: {
+        //    consumer_key: '',
+        //    consumer_secret: '',
+        //    callbackURL     : '	https://<url>/application/api/auth/twitter/callback'
+        //},
+
+        //In the below sections, consumer_Key and consumer_secret can be removed if a global login provider is connected.
+        //If using separate apps list app keys below
+        first: {
             consumer_key: 'vO5NiCxKZWa2KmNQaLBFwX2Cj',
             consumer_secret: 'NlbGlBgPmfMPgViOUo21ylNvfp5kEKYlmpfz03IVD5Ic3geQjY',
-            callbackURL     : '	https://advertise.nightpaws.eu/application/api/auth/twitter/callback'
+            access_token_key: '705761066924560384-kqAVsk3JnydZyoJnp6nVzxiudQwxaIF',
+            access_token_secret: 'eEusbHEuXFsACj8zpAbcPsWRinbo2ESknvdIf74eXZZN1'
+        },
+        second: {
+            consumer_key: '',
+            consumer_secret: '',
+            access_token_key: '',
+            access_token_secret: ''
+        },
+        third: {
+            consumer_key: '',
+            consumer_secret: '',
+            access_token_key: '',
+            access_token_secret: ''
+        },
+        fourth: {
+            consumer_key: '',
+            consumer_secret: '',
+            access_token_key: '',
+            access_token_secret: ''
+        },
+        fifth: {
+            consumer_key: '',
+            consumer_secret: '',
+            access_token_key: '',
+            access_token_secret: ''
+        },
+        phd: {
+            consumer_key: '',
+            consumer_secret: '',
+            access_token_key: '',
+            access_token_secret: ''
         }
+    },
+
+    facebook: {
+        notImplemented: ''
+    },
+
+    telegram: {
+      notImplemented: ''
     },
 
     //Logging Locations...
