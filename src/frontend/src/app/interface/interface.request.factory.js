a26e6cbdb5b82b3754bee04138132b4708960522
Nightpaws <nightpaws@users.noreply.github.com>
2016-03-07 21:03:23 +0000

    creating basic factory method for interface

--- src/frontend/src/app/interface/interface.request.factory.js
+++ src/frontend/src/app/interface/interface.request.factory.js
@@ -3,20 +3,36 @@
  */
 //Interface Factory Method
 angular.module('interface')
-    .factory('interface.request.factory', ['$http', 'config', function($http, config){
+    .factory('interface.request.factory', ['$http', 'config', function ($http, config) {
 
         var requestHelper = {};
 
-        requestHelper.getInterfaceInfo = function(){
+        requestHelper.getInterfaceInfo = function () {
 
-            var url =config.API_URL +  '/interface';
+            var url = config.API_URL + '/interface';
 
             //should maybe be using https
             return $http({
                 method: 'GET',
                 url: url
             })
+        };
 
+        requestHelper.sendMessage = function (subject, message, years) {
+            var url = config.API_URL + '/interface/post/';
+
+            return $http({
+                method: 'POST',
+                url: url,
+                sub: subject,
+                msg: message,
+                1: 1,
+                2: 2,
+                3: 3,
+                4: 4,
+                5: 5,
+                6: 6
+            })
         };
 
         return requestHelper;
