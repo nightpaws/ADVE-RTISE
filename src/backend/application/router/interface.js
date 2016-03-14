3ec195c791eabbb2292aa0545d3dededd4a63f96
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-14 02:43:10 +0000

    Twitter posting is now functional. Removing unnecessary logging

--- src/backend/application/router/interface.js
+++ src/backend/application/router/interface.js
@@ -13,12 +13,6 @@ var interface = function () {
         .post(function (req, res) {
             var data = req.body;
             var social = require('./../modules/accounts/account.action');
-            console.log('INITIAL LOGGING-------------------------');
-            console.log('INIT - ' + data.subject);
-            console.log('INIT - ' + data.message);
-            console.log('INIT - ' + data.recipients);
-            console.log('INIT - ' + data.sender);
-            console.log('END INITIAL LOGGING---------------------');
 
             var promise = social.submit(data.subject, data.message, data.recipients, data.sender);
             var response = responseFactory();
