7b250c1ff60e099692ad173771d37556aaf8fffb
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-14 22:51:28 +0000

    checking is failing. Removing possible cause for now

--- src/backend/application/modules/accounts/account.action.js
+++ src/backend/application/modules/accounts/account.action.js
@@ -14,66 +14,55 @@ var mongoose = require('mongoose');
 //External posting requirements
 var Twit = require('twit');
 
-var first, second, third, fourth, fifth, phd;
-
 //Twitter accounts for Twit
-if (config.twitter.first.consumer_key != '') {
-    first = new Twit({
-        consumer_key: config.twitter.first.consumer_key,
-        consumer_secret: config.twitter.first.consumer_secret,
-        access_token: config.twitter.first.access_token_key,
-        access_token_secret: config.twitter.first.access_token_secret,
-        timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
-    });
-}
+var first = new Twit({
+    consumer_key: config.twitter.first.consumer_key,
+    consumer_secret: config.twitter.first.consumer_secret,
+    access_token: config.twitter.first.access_token_key,
+    access_token_secret: config.twitter.first.access_token_secret,
+    timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
+});
 
-if (config.twitter.second.consumer_key != '') {
-    second = new Twit({
-        consumer_key: config.twitter.second.consumer_key,
-        consumer_secret: config.twitter.second.consumer_secret,
-        access_token: config.twitter.second.access_token_key,
-        access_token_secret: config.twitter.second.access_token_secret,
-        timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
-    });
-}
+var second = new Twit({
+    consumer_key: config.twitter.second.consumer_key,
+    consumer_secret: config.twitter.second.consumer_secret,
+    access_token: config.twitter.second.access_token_key,
+    access_token_secret: config.twitter.second.access_token_secret,
+    timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
+});
 
-if (config.twitter.third.consumer_key != '') {
-    third = new Twit({
-        consumer_key: config.twitter.third.consumer_key,
-        consumer_secret: config.twitter.third.consumer_secret,
-        access_token: config.twitter.third.access_token_key,
-        access_token_secret: config.twitter.third.access_token_secret,
-        timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
-    });
-}
+var third = new Twit({
+    consumer_key: config.twitter.third.consumer_key,
+    consumer_secret: config.twitter.third.consumer_secret,
+    access_token: config.twitter.third.access_token_key,
+    access_token_secret: config.twitter.third.access_token_secret,
+    timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
+});
 
-if (config.twitter.fourth.consumer_key != '') {
-    fourth = new Twit({
-        consumer_key: config.twitter.fourth.consumer_key,
-        consumer_secret: config.twitter.fourth.consumer_secret,
-        access_token: config.twitter.fourth.access_token_key,
-        access_token_secret: config.twitter.fourth.access_token_secret,
-        timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
-    });
-}
-if (config.twitter.fifth.consumer_key != '') {
-    fifth = new Twit({
-        consumer_key: config.twitter.fifth.consumer_key,
-        consumer_secret: config.twitter.fifth.consumer_secret,
-        access_token: config.twitter.fifth.access_token_key,
-        access_token_secret: config.twitter.fifth.access_token_secret,
-        timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
-    });
-}
-if (config.twitter.phd.consumer_key != '') {
-    phd = new Twit({
-        consumer_key: config.twitter.phd.consumer_key,
-        consumer_secret: config.twitter.phd.consumer_secret,
-        access_token: config.twitter.phd.access_token_key,
-        access_token_secret: config.twitter.phd.access_token_secret,
-        timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
-    });
-}
+var fourth = new Twit({
+    consumer_key: config.twitter.fourth.consumer_key,
+    consumer_secret: config.twitter.fourth.consumer_secret,
+    access_token: config.twitter.fourth.access_token_key,
+    access_token_secret: config.twitter.fourth.access_token_secret,
+    timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
+});
+
+var fifth = new Twit({
+    consumer_key: config.twitter.fifth.consumer_key,
+    consumer_secret: config.twitter.fifth.consumer_secret,
+    access_token: config.twitter.fifth.access_token_key,
+    access_token_secret: config.twitter.fifth.access_token_secret,
+    timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
+});
+
+var phd = new Twit({
+    consumer_key: config.twitter.phd.consumer_key,
+    consumer_secret: config.twitter.phd.consumer_secret,
+    access_token: config.twitter.phd.access_token_key,
+    access_token_secret: config.twitter.phd.access_token_secret,
+    timeout_ms: 60 * 1000  // optional HTTP request timeout to apply to all requests.
+});
+
 
 var accounts = {
     submit: function (subject, message, recipients, sender) {
