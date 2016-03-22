f94d4bbe69227f0846504ffacbc5a47f5eb3baf0
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-22 17:34:34 +0000

    invalid directory fix. Was one directory too far back

--- src/backend/application/router/message.js
+++ src/backend/application/router/message.js
@@ -2,7 +2,7 @@
  * Created by Nightpaws on 26/02/2016.
  */
 var express = require('express'),
-    responseFactory = require('./../../response/response');
+    responseFactory = require('./../response/response');
 
 var message = function () {
     
