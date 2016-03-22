184d3a9642efcd7614f0d0b9e947e7b56c047f6e
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-22 23:08:04 +0000

    Fixing missed value when renaming angular module, and fully commented table

--- src/frontend/src/app/log/log.request.factory.js
+++ src/frontend/src/app/log/log.request.factory.js
@@ -2,7 +2,7 @@
  * Created by Nightpaws on 02/03/2016.
  */
 angular.module('log')
-    .factory('schedule.request.factory', ['$http', 'config', function ($http, config) {
+    .factory('log.request.factory', ['$http', 'config', function ($http, config) {
 
         var requestHelper = {};
 
