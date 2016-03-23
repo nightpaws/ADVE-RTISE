1038b6aef7dde08a66761666336ea9db3c53ec53
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-23 11:23:15 +0000

    attempt of easy match with angular and received data.

--- src/frontend/src/app/log/log.main.controller.js
+++ src/frontend/src/app/log/log.main.controller.js
@@ -5,33 +5,14 @@ angular.module('log')
     .controller('log.main.controller', ['$scope', 'log.request.factory', 'toastr', function ($scope, requestHelper, toastr) {
 
         $scope.results = [];
-
-        //Serverside?
-        // $scope.msg = {
-        //     subject: null,
-        //     content: null,
-        //     sender: null,
-        //     year: {
-        //         y1: false,
-        //         y2: false,
-        //         y3: false,
-        //         y4: false,
-        //         y5: false,
-        //         y6: false
-        //     },
-        //     postDate: null
-        // };
-
+        
         requestHelper.getMessages()
             .then(function (data) {
                 if (data.data.successful) {
                     $scope.results = data.data.result;
 
                     toastr.info("Check console","DEVLOG");
-
-                    console.log("TESTRESULTS BELOW:");
-                    console.log($scope.results);
-                    console.log("TESTRESULTS END");
+                    
 
                 } else {
                     toastr.error(data.data.message, 'Error');
