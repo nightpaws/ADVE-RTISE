7c1c427d54efd227261a9605fec574100aee4637
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-23 10:53:53 +0000

    attempting to return values from the database table to the client

--- src/backend/application/modules/message/messages.js
+++ src/backend/application/modules/message/messages.js
@@ -27,38 +27,23 @@ var messages = {
                 var response = {
                   meta: docs
                 };
-                console.log("MESSAGE START");
-                console.log("m:" + docs.message);
-                console.log("s:" + docs.subject);
-                console.log("u" + docs.uid);
-                console.log("p" + docs.postDate);
-                console.log("y" + docs.year);
-                console.log("MESSAGE END");
+
+
+                if (response!=null){
+                    deferred.resolve(response);
+                }
+                // console.log("MESSAGE START");
+                // console.log("m:" + docs.message);
+                // console.log("s:" + docs.subject);
+                // console.log("u" + docs.uid);
+                // console.log("p" + docs.postDate);
+                // console.log("y" + docs.year);
+                // console.log("MESSAGE END");
+
+
 
             }
         });
-
-        // messageModel
-        //     .find().limit(20).sort({__id:-1})
-        //     .exec(function(err){
-        //
-        //     if(err){
-        //         deferred.reject('Error whilst retrieving messages');
-        //     }
-        //     else {
-        //
-        //         var response = {
-        //             warnings:[],
-        //             meta:{}
-        //         }
-        //
-        //         response.meta.messages = messages
-        //     }
-        // });
-
-
-
-        deferred.reject("Not yet implemented");
 
         return deferred.promise;
     }
