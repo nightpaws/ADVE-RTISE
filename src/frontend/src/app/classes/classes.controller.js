d591bc240a5f8c7e01cd008730d81aacbb93a9c6
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-24 14:52:06 +0000

    attempting to fix variable passing

--- src/frontend/src/app/classes/classes.controller.js
+++ src/frontend/src/app/classes/classes.controller.js
@@ -5,19 +5,35 @@ angular.module('classes')
     .controller('classes.controller', ['$rootScope', '$scope', 'config', function ($rootScope, $scope, config) {
         $scope.accounts = {
             //Facebook account url link
-            FBfirst: config.FB_FIRST,
-            FBsecond: config.FB_SECOND,
-            FBthird: config.FB_THIRD,
-            FBfourth: config.FB_FOURTH,
-            FBfifth: config.FB_FITH,
-            FBphd: config.FB_PHD,
+            FBfirst: null,
+            FBsecond: null,
+            FBthird: null,
+            FBfourth: null,
+            FBfifth: null,
+            FBphd: null,
             //Twitter account url link
-            TWfirst: config.TW_FIRST,
-            TWsecond:config.TW_SECOND,
-            TWthird:config.TW_THIRD,
-            TWfourth:config.TW_FOURTH,
-            TWfifth:config.TW_FIFTH,
-            TWphd:config.TW_PHD
+            TWfirst:  null,
+            TWsecond: null,
+            TWthird: null,
+            TWfourth: null,
+            TWfifth: null,
+            TWphd: null
         }
+
+        var tw1 = config.FB_FIRST;
+        var tw2= config.FB_SECOND;
+        var tw3 = config.TW_THIRD;
+        var tw4 = config.TW_FOURTH;
+        var tw5 = config.TW_FIFTH;
+        var tw6 = config.TW_PHD;
+
+
+        $scope.accounts.TWfirst = tw1;
+        $scope.accounts.TWsecond = tw2;
+        $scope.accounts.TWthird = tw3;
+        $scope.accounts.TWfourth = tw4;
+        $scope.accounts.TWfifth = tw5;
+        $scope.accounts.TWphd = tw6;
         
+
     }]);
