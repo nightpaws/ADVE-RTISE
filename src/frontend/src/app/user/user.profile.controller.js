aec3a9244370ec8be82ca8c541181d08ee698c2e
Nightpaws Darkwolf <nightpaws@users.noreply.github.com>
2016-03-25 00:58:36 +0000

    Using JS to convert lecturer instead.

--- src/frontend/src/app/user/user.profile.controller.js
+++ src/frontend/src/app/user/user.profile.controller.js
@@ -4,23 +4,30 @@
 angular.module('user')
     .controller('user.profile', ['$scope', 'user.service', function ($scope, userService) {
         const user = userService.getUser();
+
+
+
+
         $scope.username = user.username;
         $scope.sn = user.sn;
         $scope.givenName = user.givenName;
         $scope.cn = user.cn;
         $scope.email = user.email;
-        $scope.isLecturer = user.isLecturer;
+        if (user.isLecturer) {
+            $scope.isLecturer = 'Yes';
+        }
+        else{
+            $scope.isLecturer = 'No';
+        }
         $scope.isAdmin = user.isAdmin;
+        if (user.isAdmin) {
+            $scope.isAdmin = 'Yes';
+        }
+        else{
+            $scope.isAdmin = 'No';
+        }
         $scope.createdOn = user.createdOn;
         $scope.imageurl = user.imageurl;
 
-        //https://stackoverflow.com/questions/24611455/how-to-display-yes-no-instead-of-true-false-in-angularjs
-        app.filter('true_false', function () {
-            return function (text, length, end) {
-                if (text) {
-                    return 'Yes';
-                }
-                return 'No';
-            }
-        });
+
     }]);
